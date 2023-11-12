36ab2f19fe5c9b369ef63be23a942cbbaed80a05
Carlos Alberto <alberto.carlos@aluno.ifsp.edu.br>
2023-11-12 13:50:01 -0300

    Adição da tela ContasDinheiro com o Scene Builder

--- src/main/java/com/example/javafxproject/ContasDinheiroController.java
+++ src/main/java/com/example/javafxproject/ContasDinheiroController.java
@@ -10,9 +10,6 @@ public class ContasDinheiroController {
     @FXML
     private TextField txfContaNome;
 
-    @FXML
-    private TextField txfDescricao;
-
 	@FXML
     private TextField txfSaldoInicial;
 
@@ -21,16 +18,14 @@ public class ContasDinheiroController {
 
 
     @FXML
-    public void criarConta() {
+    public void criarConta()  {
         String nome = txfContaNome.getText();
-        String descricao = txfDescricao.getText();
 		double valorSaldoInicial = Double.parseDouble(txfSaldoInicial.getText());
         LocalDate dataSelecionada = dataSaldo.getValue();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String dataFormatada = dataSelecionada.format(formatter);
         
         System.out.println("Nome: " + nome);
-        System.out.println("Descrição: " + descricao);
         System.out.println("Saldo Inicial: " + valorSaldoInicial);
         System.out.println("Data de inserção: " + dataFormatada);
            

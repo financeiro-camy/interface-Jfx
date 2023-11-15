f9df07ef149480f629b8e42c12c4cedf58e60eef
Carlos Alberto <alberto.carlos@aluno.ifsp.edu.br>
2023-11-15 03:43:22 -0300

    uiuui

--- src/main/java/com/example/javafxproject/ProjetoCofrinhoController.java
+++ src/main/java/com/example/javafxproject/ProjetoCofrinhoController.java
@@ -43,4 +43,8 @@ public class ProjetoCofrinhoController {
         System.out.println("Status: " + projetoAtivo);
            
 }
+
+    public void MenuPC(){
+        System.out.println("Loading...");
+    }
 }

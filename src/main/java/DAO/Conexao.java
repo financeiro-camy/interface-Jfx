2273a57befd2c3c8c4d6e672ea6ec55a8c9e8c92
Carlos Alberto <alberto.carlos@aluno.ifsp.edu.br>
2023-11-21 13:42:06 -0300

    Por favor, trocar pelo seu endereço de database quando for executar

--- src/main/java/DAO/Conexao.java
+++ src/main/java/DAO/Conexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexao{
     
         public static Connection getConnection() {
-            String url = "jdbc:mysql://127.0.0.1:3306/Camy?user=root&password=ditto&useSSL=true";
+            String url = "jdbc:mysql://127.0.0.1:3306/Camy?user=root&password=ditto&useSSL=true"; //Trocar pelo seu endereço de conexão: jdbc:mysql://localhost/estudante1?user=estudante1&password=estudante1&useSSL=true"
             try {
                 return DriverManager.getConnection(url);
             } catch (SQLException e) {

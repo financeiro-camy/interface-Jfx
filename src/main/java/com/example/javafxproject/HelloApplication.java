02f53e9d644ddaa6c61a2df884d2e0b83c6fd1e2
Carlos Alberto <alberto.carlos@aluno.ifsp.edu.br>
2023-12-13 11:54:59 -0300

    Atualização da atualização da atualização da nova tela

--- src/main/java/com/example/javafxproject/HelloApplication.java
+++ src/main/java/com/example/javafxproject/HelloApplication.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("tela-info-pc-nova.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("tela-progressoPC.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.setTitle("Aplicação");
